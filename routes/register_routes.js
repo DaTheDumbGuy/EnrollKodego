7f2c0e5857de802914881289fef407a9f35c1fdd
Daryl Bacurin <darylbacurin14@gmail.com>
2023-04-19 20:40:13 +0800

    [Remove useless syntax]

--- routes/register_routes.js
+++ routes/register_routes.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const session = require('express-session');
 
 router.get('/', (req, res) => {
     res.render('main');
@@ -9,14 +8,6 @@ router.get('/', (req, res) => {
 router.get('/register', (req, res) => {
     res.render('register');
 });
-
-router.use(session({
-    secret: "some secret",
-    cookie: { maxAge: 30000 },
-    saveUninitialized: false,
-    resave: false
-}));
-
 
 
 module.exports = router;
